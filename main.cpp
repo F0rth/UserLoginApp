42ee2592500df25884f2de3f5f862d53d7b9506a
Denis Trofimov <silaradost@yandex.ru>
2017-06-16 16:22:21 +0300

    Load translations in main.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    QTranslator translator;
+    translator.load(QLocale::system(),
+                    "UserLoginApp", "_", ":/", ".qm");
+    app.installTranslator(&translator);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
